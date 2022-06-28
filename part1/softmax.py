5463fc1633efecf4839ba94e39be3cc404824367
Ramiro Ramirez <ramiro@kryha.io>
2022-06-28 11:47:50 +0200

    Add alt solution

--- part1/softmax.py
+++ part1/softmax.py
@@ -90,6 +90,19 @@ def run_gradient_descent_iteration(X, Y, theta, alpha, lambda_factor, temp_param
     Returns:
         theta - (k, d) NumPy array that is the final value of parameters theta
     """
+    """
+    Alt solution
+    itemp=1./temp_parameter
+    num_examples = X.shape[0]
+    num_labels = theta.shape[0]
+    probabilities = compute_probabilities(X, theta, temp_parameter)
+    # M[i][j] = 1 if y^(j) = i and 0 otherwise.
+    M = sparse.coo_matrix(([1]*num_examples, (Y,range(num_examples))), shape=(num_labels,num_examples)).toarray()
+    non_regularized_gradient = np.dot(M-probabilities, X)
+    non_regularized_gradient *= -itemp/num_examples
+    return theta - alpha * (non_regularized_gradient + lambda_factor * theta)
+    """
+
     n = X.shape[0]
     probabilities = compute_probabilities(X, theta, temp_parameter)
 
