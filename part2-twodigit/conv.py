c321b3c8eca02b757255aea8b1ea61efcaed40b1
Ramiro Ramirez <ramiro@kryha.io>
2022-07-16 16:03:26 +0200

    Add Convolutional NN

--- part2-twodigit/conv.py
+++ part2-twodigit/conv.py
@@ -11,8 +11,7 @@ batch_size = 64
 nb_classes = 10
 nb_epoch = 30
 num_classes = 10
-img_rows, img_cols = 42, 28 # input image dimensions
-
+img_rows, img_cols = 42, 28  # input image dimensions
 
 
 class CNN(nn.Module):
@@ -20,15 +19,41 @@ class CNN(nn.Module):
     def __init__(self, input_dimension):
         super(CNN, self).__init__()
         # TODO initialize model layers here
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 32, (3, 3)),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2)),
+            nn.Conv2d(32, 64, (3, 3)),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2)),
+            Flatten(),
+            nn.Linear(2880, 128),
+            nn.Dropout(0.5),
+            nn.Linear(128, 10)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(1, 32, (3, 3)),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2)),
+            nn.Conv2d(32, 64, (3, 3)),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2)),
+            Flatten(),
+            nn.Linear(2880, 128),
+            nn.Dropout(0.5),
+            nn.Linear(128, 10)
+        )
 
     def forward(self, x):
-
         # TODO use model layers to predict the two digits
-
+        out_first_digit = self.conv1(x)
+        out_second_digit = self.conv2(x)
         return out_first_digit, out_second_digit
 
+
 def main():
-    X_train, y_train, X_test, y_test = U.get_data(path_to_data_dir, use_mini_dataset)
+    X_train, y_train, X_test, y_test = U.get_data(
+        path_to_data_dir, use_mini_dataset)
 
     # Split into train and dev
     dev_split_index = int(9 * len(X_train) / 10)
@@ -40,7 +65,8 @@ def main():
     permutation = np.array([i for i in range(len(X_train))])
     np.random.shuffle(permutation)
     X_train = [X_train[i] for i in permutation]
-    y_train = [[y_train[0][i] for i in permutation], [y_train[1][i] for i in permutation]]
+    y_train = [[y_train[0][i] for i in permutation],
+               [y_train[1][i] for i in permutation]]
 
     # Split dataset into batches
     train_batches = batchify_data(X_train, y_train, batch_size)
@@ -49,14 +75,16 @@ def main():
 
     # Load model
     input_dimension = img_rows * img_cols
-    model = CNN(input_dimension) # TODO add proper layers to CNN class above
+    model = CNN(input_dimension)  # TODO add proper layers to CNN class above
 
     # Train
     train_model(train_batches, dev_batches, model)
 
-    ## Evaluate the model on test data
+    # Evaluate the model on test data
     loss, acc = run_epoch(test_batches, model.eval(), None)
-    print('Test loss1: {:.6f}  accuracy1: {:.6f}  loss2: {:.6f}   accuracy2: {:.6f}'.format(loss[0], acc[0], loss[1], acc[1]))
+    print('Test loss1: {:.6f}  accuracy1: {:.6f}  loss2: {:.6f}   accuracy2: {:.6f}'.format(
+        loss[0], acc[0], loss[1], acc[1]))
+
 
 if __name__ == '__main__':
     # Specify seed for deterministic behavior, then shuffle. Do not change seed for official submissions to edx
--- part2-twodigit/mlp.py
+++ part2-twodigit/mlp.py
@@ -11,7 +11,8 @@ batch_size = 64
 nb_classes = 10
 nb_epoch = 30
 num_classes = 10
-img_rows, img_cols = 42, 28 # input image dimensions
+img_rows, img_cols = 42, 28  # input image dimensions
+
 
 class MLP(nn.Module):
 
@@ -19,16 +20,29 @@ class MLP(nn.Module):
         super(MLP, self).__init__()
         self.flatten = Flatten()
         # TODO initialize model layers here
+        self.first_linear = nn.Sequential(
+            nn.Linear(input_dimension, 64),
+            nn.ReLU(),
+            nn.Linear(64, 10)
+        )
+        self.second_linear = nn.Sequential(
+            nn.Linear(input_dimension, 64),
+            nn.ReLU(),
+            nn.Linear(64, 10)
+        )
 
     def forward(self, x):
         xf = self.flatten(x)
-
         # TODO use model layers to predict the two digits
+        out_first_digit = self.first_linear(xf)
+        out_second_digit = self.second_linear(xf)
 
         return out_first_digit, out_second_digit
 
+
 def main():
-    X_train, y_train, X_test, y_test = U.get_data(path_to_data_dir, use_mini_dataset)
+    X_train, y_train, X_test, y_test = U.get_data(
+        path_to_data_dir, use_mini_dataset)
 
     # Split into train and dev
     dev_split_index = int(9 * len(X_train) / 10)
@@ -40,7 +54,8 @@ def main():
     permutation = np.array([i for i in range(len(X_train))])
     np.random.shuffle(permutation)
     X_train = [X_train[i] for i in permutation]
-    y_train = [[y_train[0][i] for i in permutation], [y_train[1][i] for i in permutation]]
+    y_train = [[y_train[0][i] for i in permutation],
+               [y_train[1][i] for i in permutation]]
 
     # Split dataset into batches
     train_batches = batchify_data(X_train, y_train, batch_size)
@@ -49,14 +64,16 @@ def main():
 
     # Load model
     input_dimension = img_rows * img_cols
-    model = MLP(input_dimension) # TODO add proper layers to MLP class above
+    model = MLP(input_dimension)  # TODO add proper layers to MLP class above
 
     # Train
     train_model(train_batches, dev_batches, model)
 
-    ## Evaluate the model on test data
+    # Evaluate the model on test data
     loss, acc = run_epoch(test_batches, model.eval(), None)
-    print('Test loss1: {:.6f}  accuracy1: {:.6f}  loss2: {:.6f}   accuracy2: {:.6f}'.format(loss[0], acc[0], loss[1], acc[1]))
+    print('Test loss1: {:.6f}  accuracy1: {:.6f}  loss2: {:.6f}   accuracy2: {:.6f}'.format(
+        loss[0], acc[0], loss[1], acc[1]))
+
 
 if __name__ == '__main__':
     # Specify seed for deterministic behavior, then shuffle. Do not change seed for official submissions to edx
